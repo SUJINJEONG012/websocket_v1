72a08d7f7771ed8e09dac9d3cf251bff59d50a71
angela <peekaboo32@naver.com>
2023-08-11 11:08:06 +0900

    KafkaTemplate 설정

--- src/main/java/com/sujin/CustomWebSocketHandler.java
+++ src/main/java/com/sujin/CustomWebSocketHandler.java
@@ -3,6 +3,8 @@ package com.sujin;
 import java.util.LinkedHashSet;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
@@ -12,8 +14,54 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class CustomWebSocketHandle extends TextWebSocketHandler {
+public class CustomWebSocketHandler extends TextWebSocketHandler {
 	
-	private static LinkHashSet<WebSocketSession> numSet = new LinkedHashSet<>();
+	private static LinkedHashSet<WebSocketSession> numSet = new LinkedHashSet<>();
+	
+	private final Producer producer;
+	
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		String payload = message.getPayload();
+		
+		if(numSet.size()>=3) {
+			WebSocketSession oldSession = numSet.iterator().next();
+			oldSession.sendMessage(new TextMessage("채팅이종료되었습니다."));
+			numSet.remove(numSet.iterator().next());
+		}
+		boolean isSessionAlive= false;
+		
+		for(WebSocketSession sess: numSet) {
+			if(sess.getId().equals(session.getId())) {
+				isSessionAlive = true;
+			}
+		}
+		
+		if(isSessionAlive) {
+			producer.produceMessage("KafkaTest", payload);
+		}
+	
+	}
+	
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		numSet.add(session);
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		numSet.remove(session);
+	}
+	
+	@Override
+	public void sendMessage(String payload) throws Exception{
+		for(WebSocketSession sess:numSet) {
+			TextMessage msg = new TextMessage(payload);
+			sess.sendMessage(msg);
+		}
+	}
+	
+	
 	
 }
