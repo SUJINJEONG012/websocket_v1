7122b8243d2a6922a36f9c869a3e783b93674b81
angela <peekaboo32@naver.com>
2023-08-24 17:38:40 +0900

    웹소켓 안됌

---
+++ src/main/java/com/sujin/ChatController.java
@@ -0,0 +1,33 @@
+package com.sujin;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ChatController {
+
+	@GetMapping("/")
+	public String index() {
+		return "errors";
+	}
+	
+	 @GetMapping("/{id}")
+	    public String chattingRoom(@PathVariable String id, HttpSession session, Model model){
+	        if(id.equals("guest")){
+	            model.addAttribute("name", "guest");
+	        }else if(id.equals("master")){
+	            model.addAttribute("name", "master");
+	        }else if(id.equals("loose")){
+	            model.addAttribute("name", "loose");
+	        }else{
+	            return "error";
+	        }
+	        return "chattingRoom";
+	    }
+	
+}
+
