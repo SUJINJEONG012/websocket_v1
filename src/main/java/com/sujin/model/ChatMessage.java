cfb74bac8a15ca4aafe9b2e7aabe7eb5024d6edb
angela <peekaboo32@naver.com>
2023-08-25 17:57:04 +0900

    실시간 채팅

--- src/main/java/com/sujin/model/ChatMessage.java
+++ src/main/java/com/sujin/model/ChatMessage.java
@@ -1,10 +1,15 @@
 package com.sujin.model;
 
+import java.awt.TrayIcon.MessageType;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ChatMessage {
-
+	private MessageType type;
+	private String content;
+	private String sender;
+	
 }
---
+++ src/main/java/com/sujin/model/MessageType.java
@@ -0,0 +1,8 @@
+package com.sujin.model;
+
+public enum MessageType {
+CHAT,
+JOIN,
+LEAVE
+	
+}
